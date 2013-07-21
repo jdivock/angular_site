421c97e3d516f48305c7521bcee46112042ec421
Jason Divock <jdivock@gmail.com>
2013-07-21 16:29:07 -0400

    cleaned up node server file

--- server.js
+++ server.js
@@ -1,39 +1,51 @@
 var express = require('express'),
 	Cookies = require('cookies'),
+	http = require('http'),
 	routes = require('./routes/multipost');
 
-var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', "*");
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
+// var allowCrossDomain = function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', "*");
+// 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+// 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-	next();
-}
+// 	next();
+// }
 
 // Setup the Express.js server
-var app = express.createServer();
-app.use(express.logger());
+var app = module.exports = express();
+var server = require('http').createServer(app);
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.cookieParser(process.env.COOKIE_SECRET));
 app.use(express.session({
 	secret: process.env.SESSION_SECRET
 }));
 app.use(express.methodOverride());
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
+app.use(app.router);
 
-if (process.argv[2] === 'dist') {
-	app.use(express.static(__dirname + "/dist"));
-} else {
-	app.use(express.static(__dirname + "/app"));
-	app.use(express.static(__dirname + "/.tmp"));
+// development only
+if (app.get('env') === 'development') {
+  app.use(express.errorHandler());
+  app.use(express.static(__dirname + "/app"));
+  app.use(express.static(__dirname + "/.tmp"));
 }
+
+// production only
+if (app.get('env') === 'production') {
+  // TODO
+  app.use(express.static(__dirname + "/dist"));
+};
 
 // Routes
 app.get('/multipost/evernote_login', routes.evernote_login);
 app.get('/multipost/evernote_cb', routes.evernote_cb);
 app.get('/multipost/tumblr_login', routes.tumblr_login);
 app.get('/multipost/tumblr_cb', routes.tumblr_cb);
-app.get('/multipost/getTumblrUserInfo', routes.getTumblrUserInfo);
+// app.get('/multipost/getTumblrUserInfo', routes.getTumblrUserInfo);
 
 app.get('/multipost/post', routes.checkSessions);
 app.post('/multipost/post', routes.multiPostPosts);
@@ -49,6 +61,6 @@ app.get('/multipost/sbwc', routes.sbwcLogin);
 app.get('/clear', routes.clearAll);
 app.get('/cookies', routes.cookies);
 
-var port = process.env.PORT || 3000;
-console.log("listening on http://localhost:" + port);
-app.listen(port);
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
